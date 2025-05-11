bc8718e2f9bd0aa2587199e07a136e2393bfbd9e
Francesco <83709470+FraDepe@users.noreply.github.com>
2025-05-11 19:16:15 +0200

    Starting point for the app

---
+++ lib/global.dart
@@ -0,0 +1,4 @@
+library;
+
+const defaultRingtone = "assets/Default.mp3"; 
+//const mi permette di modificare il valore dalle impostazione dell app??
---
+++ lib/model/alarm.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:i_sonno/global.dart' as globals;
+
+class Alarm {
+  TimeOfDay time;
+  List<String> days; // Da lunedì a domenica
+  bool isActive;
+  String label;
+  bool vibration;
+  String ringtonePath;
+  // bool autoDelete;
+  //qualcosa per programmare nel futuro la sveglia (oltre i sette giorni della settimana)
+
+    Alarm(
+      this.time,
+      this.days, // default oggi o domani in base all'orario OR required
+      [
+        this.isActive = true,
+        this.label = "Sveglia",
+        this.vibration = false,
+        this.ringtonePath = globals.defaultRingtone
+      ]
+    );
+    /*
+    Alarm({
+      required this.time,
+      required this.days,
+      this.isActive = true,
+      this.label = "Sveglia",
+      this.vibration = false,
+      this.ringtonePath = globals.defaultRingtone
+    });
+    */
+}
